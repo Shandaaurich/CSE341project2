374f3b20e9d630b3941643181c1005a39549fc5b
ShandaAurich <shanda@aurich.co>
2023-10-21 10:51:43 -0700

    more testing

--- Library/server.js
+++ Library/server.js
@@ -21,7 +21,7 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use((req, res, next) => {
     // res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin', 'X-Requested-With, Content-Type, Accept, Z-Key');
+    // res.setHeader('Access-Control-Allow-Headers', 'Origin', 'X-Requested-With, Content-Type, Accept, Z-Key');
     // res.setHeader('Content-Type', 'application/json');
     res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
     next();
