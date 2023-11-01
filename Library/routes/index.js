89b06b05f9cd155c25aba3958b96fd8c82a2b4ce
ShandaAurich <shanda@aurich.co>
2023-11-01 15:41:22 -0700

    adding Auth0 config to routes/index

--- Library/routes/index.js
+++ Library/routes/index.js
@@ -1,5 +1,24 @@
 const routes = require('express').Router();
 
+//Auth0 authentication
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: '7f00JPEb4i28uyhIEfraVR3vnB5IE_yF_5x-gAY_w97wZxkCiSA1EIyeXdVCy95S',
+  baseURL: 'http://localhost:8080',
+  clientID: '5X1Y6EEDOBiysAU6TcHC3jKlURwT6wAx',
+  issuerBaseURL: 'https://dev-msbr740eu250r17d.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+routes.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+routes.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
 
 routes.use('/auth', require('./auth-routes'));
 routes.use('/', require('./swagger'));
