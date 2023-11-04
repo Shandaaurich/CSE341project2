f4acd28f739b6a7621733620c221243a9adaed78
ShandaAurich <shanda@aurich.co>
2023-11-04 16:32:43 -0700

    adding requiresAuth to routes

--- Library/routes/index.js
+++ Library/routes/index.js
@@ -1,8 +1,10 @@
 const routes = require('express').Router();
+const { requiresAuth } = require('express-openid-connect');
+
 
 routes.use('/', require('./swagger'));
-routes.use('/books', require('./books'));
-routes.use('/series', require('./series'));
-routes.use('/users', require('./users'));
+routes.use('/books', requiresAuth(), require('./books'));
+routes.use('/series', requiresAuth(), require('./series'));
+routes.use('/users',  requiresAuth(), require('./users'));
 
 module.exports = routes;
